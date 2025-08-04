c5a48ebf7b2af75aa1dd90e96232ef5c7944f20a
01Kraken10 <116444755+01Kraken10@users.noreply.github.com>
2025-08-04 20:38:34 +0600

    Update HandSpriteEvents.cs

--- Assets/MyGame/Scripts/HandSpriteEvents.cs
+++ Assets/MyGame/Scripts/HandSpriteEvents.cs
@@ -1,15 +1,25 @@
 using UnityEngine;
 
+// Этот скрипт используется для вызова методов из анимации руки.
+// Он висит на дочернем объекте (спрайте), на котором находится Animator.
+// Поскольку Animator мешает перемещению объекта через DOTween,
+// его вынесли на дочерний объект, а основной логикой управляет родительский объект с компонентом Hand.
+// Методы этого скрипта вызываются как Animation Events внутри анимации.
 public class HandSpriteEvents : MonoBehaviour
 {
+    // Метод вызывается в конце промежуточной анимации анимации (нанесение косметики на кисточку)
     public void OnMakeupAnimationFinished()
     {
         GetComponentInParent<Hand>()?.GoToFinish();
     }
+
+    // Метод вызывается в момент нанесения макияжа
     public void MakeupAnimation()
     {
         GetComponentInParent<Hand>()?.MakeUp();
     }
+
+    // Метод вызывается, когда нужно вернуть руку назад
     public void GoBackAnimation()
     {
         GetComponentInParent<Hand>()?.GoToBack();
